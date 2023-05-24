298f2412034e51b8e12bbff1fdfade2e3fe34888
AlphaZer00 <batudasdemir@gmail.com>
2023-05-24 12:51:14 -0400

    Add index.html and main.js and link main.js to html

---
+++ main.js
@@ -0,0 +1,13 @@
+//----Rock, Paper, Scissors----
+//  This program is a simple rock, paper, scissors game in which the user will make a selection
+//  and that will be compared to a random selection by the computer for five rounds.
+//
+//  Planning
+//  Functions needed:
+//      - Computer's random choice
+//      - A way for the user to make a selection
+//      - Comparison between user's and computer's selection 
+//      - Function to keep score
+//      - Function to return a message that explains who won and and option to play again
+//      - Game function that loops comparison function five times and displays score
+
