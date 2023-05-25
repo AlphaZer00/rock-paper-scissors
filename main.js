26e443d4ed5f15e1de6152956db0fc5ab33f9a33
AlphaZer00 <batudasdemir@gmail.com>
2023-05-25 14:42:04 -0400

    Add round function which recieves input and compares with computer selection

--- main.js
+++ main.js
@@ -11,16 +11,58 @@
 //      - Function to return a message that explains who won and and option to play again
 //      - Game function that loops comparison function five times and displays score
 
+let computerSelection;
+
 function getComputerSelection() {
-    let computerSelection;
+    
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 1) {
-        computerSelection = "Rock";
+        computerSelection = "rock";
     } else if (randomNumber === 2) {
-        computerSelection = "Paper";
+        computerSelection = "paper";
     } else {
-        computerSelection = "Scissors";
+        computerSelection = "scissors";
     }
     return computerSelection;
 }
-console.log(getComputerSelection());
+//console.log(getComputerSelection());
+
+function round(playerSelection, computerSelection) {
+    playerSelection = prompt("Rock, Paper, or Scissors?",);
+    response = playerSelection.toLowerCase();
+    computerSelection = getComputerSelection();
+
+    console.log(computerSelection);
+
+    if ((response !== "rock" ) && (response !== "paper") && (response !== "scissors" )) {
+        alert("That is not a valid input! Please check your spelling.");
+        playerSelection = prompt("Rock, Paper, or Scissors?",);
+        response = playerSelection.toLowerCase();
+    }
+
+    console.log(response);
+
+    if (response === computerSelection) {
+        return "It's a tie!";
+    }
+
+    if (response=== "scissors" && computerSelection === "paper") {
+        return "Scissors beats paper, you win!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "Rock beats scissors, you lose!";
+    }
+
+    if (response === "rock" && computerSelection === "scissors") {
+        return "Rock beats scissors, you win!";
+    } else if (response === "rock" && computerSelection === "paper") {
+        return "Paper beats rock, you lose!";
+    }
+
+    if (response === "paper" && computerSelection === "rock") {
+        return "Paper beats rock, you win!"
+    } else if (response === "paper" && computerSelection === "scissors") {
+        return "Scissors beats paper, you lose!"
+    }
+}
+console.log(round());
+
