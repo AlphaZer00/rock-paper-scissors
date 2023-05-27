261ac5a9c07164bf3e07d43caa71ac696a9add27
AlphaZer00 <batudasdemir@gmail.com>
2023-05-27 09:58:54 -0400

    Change indenting to improve readability

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-//----Rock, Paper, Scissors----
-//  This program is a simple rock, paper, scissors game in which the user will make a selection
-//  and that will be compared to a random selection by the computer for five rounds.
-//
-//  Planning
-//  Functions needed:
-//      - Computer's random choice
-//      - A way for the user to make a selection
-//      - Comparison between user's and computer's selection 
-//      - Function to keep score
-//      - Function to return a message that explains who won and and option to play again
-//      - Game function that loops comparison function five times and displays score
+/*                               ----Rock, Paper, Scissors----
+  This program is a simple rock, paper, scissors game in which the user will make a selection
+  and that will be compared to a random selection by the computer for five rounds.
+
+  Planning
+  Functions needed:
+      - Computer's random choice
+      - A way for the user to make a selection
+      - Comparison between user's and computer's selection 
+      - Function to keep score
+      - Function to return a message that explains who won and and option to play again
+      - Game function that loops comparison function five times and displays score
+*/
 
 let computerSelection;
 let outcome;
@@ -17,6 +18,7 @@ let outcome;
 function getComputerSelection() {
     
     let randomNumber = Math.floor(Math.random() * 3) + 1;
+
     if (randomNumber === 1) {
         computerSelection = "rock";
     } else if (randomNumber === 2) {
@@ -24,6 +26,7 @@ function getComputerSelection() {
     } else {
         computerSelection = "scissors";
     }
+
     return computerSelection;
 }
 
@@ -79,17 +82,19 @@ function playGame() {
     let computerScore = 0;
     
     for (let i = 0; i < 5; i++) {
-    playRound();
-    if (outcome.slice(-5) === "lose!") {
-        computerScore = ++computerScore;
-    } else if (outcome.slice(-4) === "win!") {
-        userScore = ++userScore;
-    } 
+        playRound();
+        if (outcome.slice(-5) === "lose!") {
+            computerScore = ++computerScore;
+        } else if (outcome.slice(-4) === "win!") {
+            userScore = ++userScore;
+        }
 
     console.log(userScore);
+
     let scoreCounter =
     `Your Score: ${userScore}
 Computer Score: ${computerScore}`;
+
     console.log(scoreCounter);
     }
 
