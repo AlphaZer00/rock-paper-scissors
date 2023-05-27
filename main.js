296eb0ef84bd233a5fb98a2307de766401dfc609
AlphaZer00 <batudasdemir@gmail.com>
2023-05-27 08:13:52 -0400

    Add 'game' function, plays 5 rounds & reports winner/loser

--- main.js
+++ main.js
@@ -12,6 +12,7 @@
 //      - Game function that loops comparison function five times and displays score
 
 let computerSelection;
+let outcome;
 
 function getComputerSelection() {
     
@@ -43,26 +44,63 @@ function round(playerSelection, computerSelection) {
     console.log(response);
 
     if (response === computerSelection) {
-        return "It's a tie!";
+        outcome = "It's a tie!";
+        return outcome;
     }
 
-    if (response=== "scissors" && computerSelection === "paper") {
-        return "Scissors beats paper, you win!";
+    if (response === "scissors" && computerSelection === "paper") {
+        outcome = "Scissors beats paper, you win!";
+        return outcome;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "Rock beats scissors, you lose!";
+        outcome = "Rock beats scissors, you lose!";
+        return outcome;
     }
 
     if (response === "rock" && computerSelection === "scissors") {
-        return "Rock beats scissors, you win!";
+        outcome = "Rock beats scissors, you win!";
+        return outcome;
     } else if (response === "rock" && computerSelection === "paper") {
-        return "Paper beats rock, you lose!";
+        outcome = "Paper beats rock, you lose!";
+        return outcome;
     }
 
     if (response === "paper" && computerSelection === "rock") {
-        return "Paper beats rock, you win!"
+        outcome = "Paper beats rock, you win!"
+        return outcome;
     } else if (response === "paper" && computerSelection === "scissors") {
-        return "Scissors beats paper, you lose!"
+        outcome = "Scissors beats paper, you lose!";
+        return outcome;
     }
 }
-console.log(round());
 
+
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+    
+    for (let i = 0; i < 5; i++) {
+    round();
+    if (outcome.slice(-5) === "lose!") {
+        computerScore = ++computerScore;
+    } else if (outcome.slice(-4) === "win!") {
+        userScore = ++userScore;
+    } 
+
+    console.log(userScore);
+    let scoreCounter =
+    `Your Score: ${userScore}
+Computer Score: ${computerScore}`;
+    console.log(scoreCounter);
+    }
+
+    if (userScore > computerScore) {
+        message = "Game over, You won!";
+        console.log(message);
+    } else if (userScore < computerScore) {
+        message = "Game over, You lost!";
+        console.log(message);
+    } else {
+        message = "Game over! It's a tie!"
+        console.log(message);
+    }
+}
